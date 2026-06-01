030f23a89a21cd00d112f58724d51195c9953b25
André Ijiri Ribeiro <andre.ijiri.ribeiro@gmail.com>
2026-06-01 11:21:01 -0300

    Update program.h
    Atualizado program.h para implementação de hashBin

--- src/program.h
+++ src/program.h
@@ -2,6 +2,7 @@
 #define _PROGRAM_H
 
 #include "params.h"
+#include "hashBin.h"
 
 /**
  * Este módulo é responsável por implementar as funções de inicialização e encerramento do programa.
@@ -17,17 +18,21 @@
  * o objeto de Parametro, as estruturas de dados para armazenar os dados dos arquivos .geo e .pm, e assim por diante.
  * 
  * @param param     Ponteiro para o objeto criado de Parametro
- * @return      0 em caso de sucesso. -1 em caso de erro
+ * @param htq       Ponteiro para a tabela hash criada para armazenar os dados do arquivo .geo
+ * @param q         Ponteiro para o objeto criado para armazenar os dados lidos do arquivo .geo
+ * @return          0 em caso de sucesso. -1 em caso de erro
  */
-int bootProgram(Param** param);
+int bootProgram(Param** param, HashBin** htq, Quadras** q);
 /**
  * Esta função é responsável por liberar a memória alocada para os objetos criados durante a execução do programa, 
  * como por exemplo, o objeto de Parametro, as estruturas de dados para armazenar os dados dos arquivos .geo e .pm, e assim por diante.
  * 
  * @param param Ponteiro para o objeto de Parametro
+ * @param htq   Ponteiro para a tabela hash criada para armazenar os dados do arquivo .geo
+ * @param q     Ponteiro para o objeto criado para armazenar os dados lidos do arquivo .geo
  * @return      0 em caso de sucesso. -1 em caso de erro
  */
-int shutProgram(Param** param);
+int shutProgram(Param** param, HashBin** htq, Quadras** q);
 
 /*###############################################################################################*/
 
